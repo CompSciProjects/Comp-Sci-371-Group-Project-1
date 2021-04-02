c64f8aafc3cddad4f04c9f5ef3a510328b63a659
CompSciProjects <81193192+CompSciProjects@users.noreply.github.com>
2021-04-02 13:11:31 -0500

    Update Administration.h

--- Administration.h
+++ Administration.h
@@ -1,3 +1,15 @@
 #ifndef ADMINISTRATION_H
+#define ADMINISTRATION_H
 
+class Admin
+{
+private:
+public:
+	void AdminMenu();
+	void AddVehicle();
+	void ViewReservation();
+	void EditReservation();
+	void ChangeCharge();
+	void ViewIncome();
+};
 #endif
