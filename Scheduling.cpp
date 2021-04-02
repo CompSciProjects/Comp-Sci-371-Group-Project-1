dd9f9d44bba1634c95b3fb083f74743d86d177fb
CompSciProjects <81193192+CompSciProjects@users.noreply.github.com>
2021-04-02 13:06:29 -0500

    Update Scheduling.cpp

--- Scheduling.cpp
+++ Scheduling.cpp
@@ -1,11 +1,12 @@
 #include "Scheduling.h"
 #include <iostream>
+using namespace std;
 
 /*To  any  destination,  the  company  intends  to  operate  at  least  3  services   a  week  
 to  and  from  any  destination. However, depending on the demand, the company can operate extra 
 service providing that there is a bus available. 
 The demand should meet at least 50% of particular bus type. 
 When you schedule  one  bus  for  a  particular  destination,  make  sure  you  have  an  available  
-bus  at  that  depot.  You  have  11  buses  and  6  destinations.  Therefore,  depending  on  the  passengers’  reservation,  
+bus  at  that  depot.  You  have  11  buses  and  6  destinations.  Therefore,  depending  on  the  passengersâ€™  reservation,  
 there  must be one bus available to complete the travel. 
 You may consider keeping an inventory of vehicles located in different depots. */
