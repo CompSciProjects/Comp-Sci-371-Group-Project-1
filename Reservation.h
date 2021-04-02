844a10f10398751596ff5ab13ead0460dcb4c9cc
CompSciProjects <81193192+CompSciProjects@users.noreply.github.com>
2021-04-02 13:07:02 -0500

    Update Reservation.h

--- Reservation.h
+++ Reservation.h
@@ -1,3 +1,12 @@
 #ifndef RESERVATION_H
-
+#define RESERVATION_H
+class Reservation
+{
+private:
+public:
+	void CustomerMenu();
+	void SeatReservation();
+	void BusHire();
+	void CancelReservation();
+};
 #endif
