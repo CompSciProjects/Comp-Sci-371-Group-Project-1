ce6adfad9191d0a93f91d82791a8767890b9cb09
CompSciProjects <81193192+CompSciProjects@users.noreply.github.com>
2021-04-22 16:56:37 -0500

    Update Administration.h

--- Administration.h
+++ Administration.h
@@ -4,12 +4,16 @@
 class Admin
 {
 private:
+	int luxBusNum = 0;
+	int miniBusNum = 0;
+	int miniVanNum = 0;
+	int changeChoice = 0;
+	double secDeposit = 0;
+	double mileCost = 0;
+	double changeAmount = 0; //rent rate
+	double changeAmountWindow = 0;
 public:
+	//reference to all methods from Administration.cpp
 	void AdminMenu();
-	void AddVehicle();
-	void ViewReservation();
-	void EditReservation();
-	void ChangeCharge();
-	void ViewIncome();
 };
 #endif
