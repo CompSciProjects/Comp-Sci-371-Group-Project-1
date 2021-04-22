44677b1a53b529d0f857543a3d8d6fbb906482b2
CompSciProjects <81193192+CompSciProjects@users.noreply.github.com>
2021-04-22 16:58:22 -0500

    Update Rates.h

--- Rates.h
+++ Rates.h
@@ -23,20 +23,6 @@ private:
 	double amount;
 	string reason;
 public:
-	//Constant seat cost rates
-	const double LUXURYAISLEANDWINDOW = 0.95; //In  luxury  bus, aisle  and  window  seat  costs   $0.95  per  mile
-	const double LUXURYOTHER = 0.75;		  //the other seat  costs  $0.75 per mile
-	const double MINIBUSSEAT = 0.65;		  //In small bus, both the aisle seat and window seat costs 0.65
-	const double MINIVANSEAT = 0.5;				  //In case of the minivan, each seat cost $0.50 per  mile
-
-	//Bus Hire rates
-	double LUXURYBH[3] = { 1500, 0.25, 5000 };   //In  case  of  bus  hire,  the  luxury  bus  cost  $1500 as rent with additional $0.25 for each  mile  of travel. 
-												//The  security  deposit  for  luxury  bus  is  $5000
-	double MINIBUSBH[3] = { 1300, 0.2, 3000 };  //The  small  bus  costs   $1300  as  rent  with  additional  $0.20  per  mile
-												//The  security  deposit for  the  small  bus  $3000 
-	double MINIVANBH[3] = { 1000, 0.15, 1500 };	    //the minivan  costs  $1000  as  rent  with  additional  $0.15  per  mile
-												//The security deposit for  the  minivan  it  is  $1500  
-
 	//Trip Distances
 	/*Source		Destination		Distance
 	Green Bay	Madison			135.6 miles
@@ -47,7 +33,7 @@ public:
 	double Distances[5] = { 135.6, 118.7, 142.2, 50.4, 194.4 };
 
 	//Reasons for transactions
-	string Reasons[3] = { "Standard Seat Reservations", "Bus Hire", "Cancel Reservation" };
+	string Reasons[4] = { "Standard Seat Reservations", "Bus Hire", "Cancel Reservation", "Company Authorized Cancellation" };
 
 	//Member Functions
 	double GetRate(int* Tickets, int BusType, bool IsBusHire);
@@ -55,6 +41,16 @@ public:
 	double GetDeposit(bool IsBusHire, int BusType);
 	double GetPenalty(Date Day, string Reason);
 	double GetAmount(Rates Rates, bool IsBusHire, int BusType);
+	double setLuxSeatRate(double changeAmountWindow, double changeAmountOther);
+	double setMiniBusSeatRate(double changeAmount);
+	double setMiniVanSeatRate(double changeAmount);
+	double setLuxBusRate(double changeAmount, double mileCost, double secDeposit);
+	double setMiniBusRate(double changeAmount, double mileCost, double secDeposit);
+	double setMiniVanRate(double changeAmount, double mileCost, double secDeposit);
+	double getLuxSeatRate();
+	double getLuxSeatRateWindow();
+	double getMiniBussSeatRate();
+	double getMiniVanSeatRate();
 	void SaveTransaction(Reservation Reservation, int Destination, int BusType, string Reason);
 	bool FindTransaction(string Name, Date Day, string Reason);
 	Rates GetTransaction(string Name, Date Day, string Reason);
