a359ba8ecf1e88d63884991fcb760b165c7babcc
CompSciProjects <81193192+CompSciProjects@users.noreply.github.com>
2021-04-27 21:20:01 -0500

    Update Scheduling.h

--- Scheduling.h
+++ Scheduling.h
@@ -1,4 +1,3 @@
-#pragma once
 #ifndef SCHEDULING_H
 #define SCHEDULING_H
 #include "Reservation.h"
@@ -7,29 +6,55 @@ using namespace std;
 class Scheduling
 {
 private:
-	int activeLuxuryBus = 0;
-	int activeMiniBus = 0;
-	int activeMiniVan = 0;
-	int activeTotalBus = 0;
-	int activeServiceWeekend[54];
+	long busID;
+	string busType;
+	string destination;
+	Date tripDate;
+	int seatsAvailable;
+	char seatArray[12][5];
 public:
-	Scheduling();
-	Scheduling(int, int, int, int);
-	int getActiveLuxuryBus();
-	int getActiveMiniBus();
-	int getActiveMiniVan();
-	int addLuxuryBus();
-	int addMiniBus();
-	int addMiniVan();
-	int getLuxuryBusTotal();
-	int getMiniBusTotal();
-	int getMiniVanTotal();
-	void setActiveLuxuryBus(int);
-	void setActiveMiniBus(int);
-	void setActiveMiniVan(int);
-	int weekNumberCheck(Reservation Reservation);
-	void weekCheck(Reservation Reservation);
-	void demandCheck(Reservation Reservation);
-	void busListCheck(Reservation Reservation);
+	int getBusCount(int BusType = -1);
+	void addVehicle(int BusType);
+	long isVehicleAvailable(string BusType, Date date);
+	bool getTrip(string Destination, string BusType, Date date, int Tickets);
+	void findTrip(long BusID, Date date);
+	void findTrip(string Destination, string BusType, Date date, int Tickets);
+	void findTrip(long BusID, string Destination, string BusType, Date date);
+	void createTrip(long BusID, string Destination, string BusType, Date date);
+	void deleteTrip(long BusID, string Destination, string BusType, Date date);
+	void saveTrip();
+	long getBusID();
+	void printSeatArray();
+	void printSeatRow(int row);
+	char getSeat(int row, int col);
+	void setSeat(int row, int col);
+	void resetSeat(int row, int col);
+	double checkCapacity();
+	void CancelUnderCapacityTrips();
+	void CancelReservationByBusByDate(long BusID, Date date);
+	long getBusIDList();
+
+	const Scheduling operator=(const Scheduling& right)
+	{
+		if (this != &right)
+		{
+			this->busID = right.busID;
+			this->busType = right.busType;
+			this->tripDate = right.tripDate;
+			this->seatsAvailable = right.seatsAvailable;
+
+			for (int i = 0; i < 12; i++)
+			{
+				for (int j = 0; j < 5; j++)
+				{
+					this->seatArray[i][j] = right.seatArray[i][j];
+				}
+			}
+		}
+
+
+		return *this;
+	}
+
 };
 #endif
