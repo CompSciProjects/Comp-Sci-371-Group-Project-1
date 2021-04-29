d1c3ed024ddb3a7ebc55b9183ad3a5923c1a16d6
CompSciProjects <81193192+CompSciProjects@users.noreply.github.com>
2021-04-29 16:56:02 -0500

    Update FlixBus_Test.cpp

--- FlixBus_Test.cpp
+++ FlixBus_Test.cpp
@@ -12,26 +12,29 @@ int main()
 	//Allows user to choose a menu option or exit the system
 	while(option != 3)
 	{
-		
 		cout << "1. Administrator Menu\n"
 			<< "2. Customer Menu\n"
 			<< "3. Exit\n"
-			<< "Please select an option 1, 2, or 3:";
+			<< "Please select an option 1, 2, or 3: ";
 		cin >> option;
 		cout << "\n";
 
 		switch (option)
 		{
 		case 1:
+
 			Admin().AdminMenu();
 			break;
 		case 2:
+
 			Reservation().CustomerMenu();
 			break;
 		case 3:
+
 			cout << "Thank you for using the FlixBus automated system! Goodbye!\n\n";
 			break;
 		default:
+
 			cout << "That is not a valid option.\n\n";
 			cin.clear();
 			cin.ignore(std::numeric_limits<std::streamsize>::max(), '\n');
